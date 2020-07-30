ac9eecbadb395067e071ccca9adf87a96d1529a5
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-30 11:20:27 -0400

    Created Main Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-function App() {
+import Homefeed from "./components/HomeeFeed";
+import Notifications from "./components/Notifications";
+import Bookmarks from "./components/Bookmarks";
+import TweetDetails from "./components/TweetDetails";
+import Profile from "./components/Profile";
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <Homefeed />
+          </Route>
+          <Route exact path="/notifications">
+            <Notifications />
+          </Route>
+          <Route exact path="/bookmarks">
+            <Bookmarks />
+          </Route>
+          <Route exact path="/tweet/:tweetId">
+            <TweetDetails />
+          </Route>
+          <Route exact path="/:profileId">
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
