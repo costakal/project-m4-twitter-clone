9a24c371ff406935b33f104bf4f2d76bce6fe683
Costa Kalalafatidis <costa.kal@hotmail.com>
2020-07-30 14:11:10 -0400

    Sidebar Styling - Complete

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,29 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 import { FiHome, FiUser, FiBell, FiBookmark } from "react-icons/fi";
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import { ReactComponent as Logo } from "../assets/logo.svg";
+import { COLORS } from "../constants";
 
 const Sidebar = () => {
   return (
     <Wrapper>
-      <Logo />
-      <Link to="/">
-        <FiHome />
-        <span>Home</span>
-      </Link>
-      <Link to="/:profileId">
-        <FiUser />
-        <span>Profile</span>
-      </Link>
-      <Link to="/notifications">
-        <FiBell />
-        <span>Notifications</span>
-      </Link>
-      <Link to="/bookmarks">
-        <FiBookmark />
-        <span>Bookmarks</span>
-      </Link>
+      <Logo style={{ margin: "15px 0px" }} />
+      <NavLink exact to="/" activeStyle={{ color: COLORS.primary }}>
+        <button>
+          <FiHome />
+          <span>Home</span>
+        </button>
+      </NavLink>
+      <NavLink to="/:profileId" activeStyle={{ color: COLORS.primary }}>
+        <button>
+          <FiUser />
+          <span>Profile</span>
+        </button>
+      </NavLink>
+      <NavLink to="/notifications" activeStyle={{ color: COLORS.primary }}>
+        <button>
+          <FiBell />
+          <span>Notifications</span>
+        </button>
+      </NavLink>
+      <NavLink to="/bookmarks" activeStyle={{ color: COLORS.primary }}>
+        <button>
+          <FiBookmark />
+          <span>Bookmarks</span>
+        </button>
+      </NavLink>
     </Wrapper>
   );
 };
@@ -31,19 +40,34 @@ const Sidebar = () => {
 export default Sidebar;
 
 const Wrapper = styled.div`
+  padding: 0px 15px 0px 150px;
   display: flex;
   flex-direction: column;
   height: 100vh;
-  width: 400px;
+  width: 230px;
   z-index: 1;
   border-right: 1px solid grey;
-  text-align: right;
   a {
     display: flex;
-    margin: 5;
-    padding: 15px;
+    cursor: default;
     text-decoration: none;
     color: black;
+    button {
+      cursor: pointer;
+      background: none;
+      border: none;
+      font-size: 20px;
+      font-weight: bold;
+      border-radius: 25px;
+      margin: 5px;
+      padding: 15px;
+      color: inherit;
+      outline: none;
+      &:hover {
+        color: ${COLORS.primary};
+        background: ${COLORS.secondary};
+      }
+    }
     span {
       margin-left: 20px;
     }
--- client/src/constants.js
+++ client/src/constants.js
@@ -1,6 +1,7 @@
 export const COLORS = {
   // Bright purple:
   primary: "hsl(258deg, 100%, 50%)",
+  secondary: "hsl(258deg, 100%, 85%)",
 
   // Add more colors as needed!
 };
